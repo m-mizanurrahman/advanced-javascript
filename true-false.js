5ebd38ad971a838c2143b67a3e9ccc043a88d182
Mizanur Rahman <mizan38ju@gmail.com>
2021-02-02 08:56:15 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,26 @@
+// // falsy-value
+// NaN
+// false
+// 0
+// '', ""
+// null
+// undefined
+
+
+// // truthy-value
+// []
+// {}
+// '0'
+// 'false'
+// all true condition
+// ' '
+// " "
+
+let names = false;
+console.log(names);
+if(names){
+    console.log('true');
+}
+else{
+    console.log('false');
+}
