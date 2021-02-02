5b281614a17c093d7457ba5b22970d96f83825d1
Mizanur Rahman <mizan38ju@gmail.com>
2021-02-02 10:39:47 +0600

    double vs tripple equal

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,18 +1,25 @@
-let pakhi;
-function add(num1, num2){
-    console.log(num1, num2);
-    return
-}
-function add(num1, num2) {
-    console.log(num1, num2);    
-}
+// let pakhi;
+// function add(num1, num2){
+//     console.log(num1, num2);
+//     return
+// }
+// function add(num1, num2) {
+//     console.log(num1, num2);    
+// }
 
-const premik = {name:"smart dude", phone: 458421};
-console.log(premik.gf);
+// const premik = {name:"smart dude", phone: 458421};
+// console.log(premik.gf);
 
-let fun = undefined;
-console.log(fun);
+// let fun = undefined;
+// console.log(fun);
 
-let ages = [2, 5, 9];
-console.log(ages[11]);
+// let ages = [2, 5, 9];
+// console.log(ages[11]);
 
+var TestVar;
+console.log(TestVar); //shows undefined
+console.log(typeof TestVar); //shows undefined
+
+var TestVar = null;
+console.log(TestVar); //shows null
+console.log(typeof TestVar); //shows object
